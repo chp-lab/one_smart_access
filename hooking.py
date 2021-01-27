27748744dbd1701592e2950c01abc0b5713b641c
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-27 19:04:39 +0700

    add webhook

---
+++ hooking.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource, reqparse
+from flask import Flask, request, jsonify
+import time
+from database import Database
+
+class Hooking(Resource):
+    def post(self):
+        TAG = "Hooking:"
+        data = request.json
+        print(TAG, data)
+
+        return {
+            "type": True,
+            "message": "success",
+            "elapsed_time_ms": elapsed_time,
+            "len": 0,
+            "result": "testing"
+        }
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from  flask_restful import Api,Resource
 from flask_cors import CORS
 from booking import Booking
 from check_perm import Check_perm
+from hooking import Hooking
 
 class Server:
     app = None
@@ -21,4 +22,6 @@ if(__name__ == "__main__"):
     server = Server()
     server.api.add_resource(Check_perm, API_VERSION + "/check_perm/<room_num>")
     server.api.add_resource(Booking, API_VERSION + "/booking/<booking_number>")
-    server.app.run(host="0.0.0.0", debug=True, port=5000)
+    server.api.add_resource(Hooking, API_VERSION + "/hooking")
+    
+    server.app.run(host="0.0.0.0", debug=True, port=5003)

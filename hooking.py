0522735fba030ec71bc64288b4095bd323de0618
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-28 10:03:30 +0700

    release test bot

--- hooking.py
+++ hooking.py
@@ -1,13 +1,32 @@
 from flask_restful import Resource, reqparse
 from flask import Flask, request, jsonify
-import time
-from database import Database
+import requests
+# -- coding: utf-8 --
 
 class Hooking(Resource):
     def post(self):
         TAG = "Hooking:"
+        url = 'https://chat-api.one.th/message/api/v1/push_quickreply'
         data = request.json
         print(TAG, data)
+        if(data['event'] == "message"):
+            bot_id = data['bot_id']
+            user_id = data['source']['user_id']
+            req_body = {
+	"to": "U18a33b1dcddf59da8000068357b3c745",
+	"bot_id": "B75900943c6205ce084d1c5e8850d40f9",
+	"message": "โปรดเลือกบริการ",
+	"quick_reply": [ {
+	"label": "ระบบจองห้องประชุม",
+	"type": "webview",
+	"url": "https://web-meeting-room.herokuapp.com/",
+	"size": "tall",
+	"sign": "false",
+	"onechat_token": "true"
+    }]
+}
+        result = requests.post(url, data=req_body)
+        print(TAG, result.text)
 
         return {
             "type": True,

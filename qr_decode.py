1e7eb5656c04ce21d4c8856fa13db05a3bd67c1b
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-29 15:49:15 +0700

    developing

--- qr_decode.py
+++ qr_decode.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 from flask import send_from_directory
 import requests
 
-class Qr_decoder(Resource):
+class Qr_decode(Resource):
     def get(self):
         TAG = "qr_decode:"
         return send_from_directory("./public", "hello.txt")

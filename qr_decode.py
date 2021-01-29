6c7bd5b2572940a4396a6972b6c00f446aec6ead
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-29 16:10:42 +0700

    developing

--- qr_decode.py
+++ qr_decode.py
@@ -10,5 +10,5 @@ class Qr_decode(Resource):
         TAG = "qr_code_hd:"
         uploaded_file = request.files['file']
         if(uploaded_file.filename != ''):
-            uploaded_file.save("tmpqrcode.png")
+            uploaded_file.save("./public/tmpqrcode.png")
             return "testing"

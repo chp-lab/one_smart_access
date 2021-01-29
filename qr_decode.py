bd58150a1a5e72275c227d69cbba5c04c5c2de35
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-01-29 16:13:14 +0700

    developing

--- qr_decode.py
+++ qr_decode.py
@@ -3,12 +3,14 @@ from flask import send_from_directory, request
 import requests
 
 class Qr_decode(Resource):
+    file_name = "tmpqrcode.jpg"
+    file_path = "./public"
     def get(self):
         TAG = "qr_decode:"
-        return send_from_directory("./public", "tmpqrcode.png")
+        return send_from_directory(self.file_path, self.file_name)
     def post(self):
         TAG = "qr_code_hd:"
         uploaded_file = request.files['file']
         if(uploaded_file.filename != ''):
-            uploaded_file.save("./public/tmpqrcode.png")
+            uploaded_file.save(self.file_path + "/" + self.file_name)
             return "testing"

2e61b72f873a12336bcb496d3caa26707b50bfac
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-03-03 18:17:41 +0700

    add one_id to users table

--- monitor.py
+++ monitor.py
@@ -7,19 +7,31 @@ from hooking import Hooking
 class Monitor():
     def job(self):
         TAG = "Job:"
-        print(TAG, "Hello")
+        alarm_start = 4
+        alarm_end = 5
         cmd = """SELECT bookings.booking_number,users.one_id, bookings.one_email, bookings.meeting_start, bookings.meeting_end, bookings.agenda, bookings.room_num,
         (CURRENT_TIMESTAMP) AS cur_time, (TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60 AS minute_before_start, DATE_ADD(bookings.meeting_start, INTERVAL 30*60 SECOND) booking_time_out
         FROM bookings
         LEFT JOIN users ON bookings.one_email=users.one_email
-        WHERE bookings.meeting_end > (CURRENT_TIMESTAMP) AND bookings.eject_at IS NULL AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) > 5)
-        AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) <= 6)"""
+        WHERE bookings.meeting_end > (CURRENT_TIMESTAMP) AND bookings.eject_at IS NULL AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) > %s)
+        AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) <= %s)""" %(alarm_start, alarm_end)
+
+        cmd_guest = """SELECT guests.booking_number, users.one_id, guests.guest_email, bookings.meeting_start, bookings.meeting_end, bookings.agenda, bookings.room_num, (CURRENT_TIMESTAMP) AS cur_time, (TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60 AS minute_before_start, DATE_ADD(bookings.meeting_start, INTERVAL 30*60 SECOND) booking_time_out
+        FROM guests
+        LEFT JOIN users ON guests.guest_email=users.one_email
+        LEFT JOIN bookings ON guests.booking_number=bookings.booking_number
+        WHERE bookings.meeting_end > (CURRENT_TIMESTAMP) AND bookings.eject_at IS NULL AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) > %s)
+        AND (((TIME_TO_SEC(bookings.meeting_start) - TIME_TO_SEC((CURRENT_TIMESTAMP)))/60) <= %s)""" %(alarm_start, alarm_end)
 
         database = Database()
         hooking = Hooking()
 
         res = database.getData(cmd)
+        res2 = database.getData(cmd_guest)
+
         bookings = res[0]['result']
+        guests = res2[0]['result']
+
         # print(TAG, "res=", res)
         print(TAG, "bookings=", bookings)
         for book in bookings:
@@ -27,6 +39,12 @@ class Monitor():
             one_id = book['one_id']
 
             reply_msg = """การจองเลขที่ %s ของคุณกำลังจะถึงเวลาเริ่ม %s สิ้นสุดเวลา %s ห้อง %s เหตุผล %s กรุณาเข้าห้องก่อนเวลา %s""" %(book['booking_number'], book['meeting_start'], book['meeting_end'], book['room_num'], book['agenda'], book['booking_time_out'])
+            hooking.send_msg(one_id, reply_msg)
+
+        for book in guests:
+            print(TAG, "guest=", book)
+            one_id = book['one_id']
+            reply_msg = """คำเชิญจากเลขที่การจอง %s กำลังจะถึงเวลาเริ่ม %s สิ้นสุดเวลา %s ห้อง %s เหตุผล %s เรียนเชิญเข้าร่วมค่ะ""" %(book['booking_number'], book['meeting_start'], book['meeting_end'], book['room_num'], book['agenda'])
             hooking.send_msg(one_id, reply_msg)
 
         return False

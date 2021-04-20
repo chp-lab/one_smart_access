53dc12937f0c2ce37181fb28c672d5d16c4198f7
chatpeth.ke <chatpeth.ke@inet.co.th>
2021-04-20 17:37:40 +0700

    change my sql config

--- mysql_config.py
+++ mysql_config.py
@@ -3,7 +3,10 @@ import yaml
 class MysqlConfig:
     mysql_conf_file = ""
     def __init__(self):
-        self.mysql_conf_file = "./database_config.yaml"
+        TAG = "mysql_config:"
+        my_dir = "/home/user01/one_smart_access"
+        print(TAG, "current dir=", my_dir)
+        self.mysql_conf_file = my_dir + "/database_config.yaml"
     def showData(self):
         print("Testing")
     def callDBConfig(self):
